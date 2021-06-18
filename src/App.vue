8ffd65954f604e2abb69b28520815f7b79601146
Redho-Prasetyo <tioartcode@gmail.com>
2021-06-18 14:01:44 +0700

    calculator

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <Heading/>
+  <Calculator/>
+</template>
+
+<script>
+import Heading from "./components/Heading.vue";
+import Calculator from "./components/Calculator.vue";
+
+export default {
+  name: "App",
+  components: {
+    Heading,
+    Calculator
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 30px;
+}
+</style>
